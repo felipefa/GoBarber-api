2bf4c282cb10bdf73bf949df6fbab65467a58aac
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-03 21:32:59 -0300

    Created route to list providers

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -15,6 +16,8 @@ routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
+
+routes.get('/providers', ProviderController.index);
 
 routes.put('/users', UserController.update);
 
