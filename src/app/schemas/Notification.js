803e1cf050576a6c0fd1e4e6ea716697af879398
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-04 22:11:28 -0300

    Created notification schema

---
+++ src/app/schemas/Notification.js
@@ -0,0 +1,24 @@
+import Mongoose from 'mongoose';
+
+const NotificationSchema = new Mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default Mongoose.model('Notification', NotificationSchema);
