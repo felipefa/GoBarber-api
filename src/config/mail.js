a00b5d5334ff7e97adf8efe256a25974b5a670e3
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-08 11:09:35 -0300

    Added nodemailer and configured email notification for canceled appointments

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: '',
+  auth: {
+    user: '38b84314a1d35b',
+    pass: '92103f7ae16848',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
