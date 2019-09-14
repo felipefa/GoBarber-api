8904b75a450ef4f31240bbdbcc36383a020b1dfe
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-13 22:34:47 -0300

    Added virtual fields for appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -17,7 +17,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit,
       offset: (page - 1) * limit,
       include: [
