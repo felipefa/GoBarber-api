8c872b8181f0fa0093150819b2d822934af8a8cb
Felipe Araujo <devaraujofelipe@gmail.com>
2019-11-16 15:55:42 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
@@ -24,6 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
